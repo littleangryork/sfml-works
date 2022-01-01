0bbe37938b07b4900f91cb939dfefb7bae3f404d
littleangryork <PQDEmMisha21>
2022-01-01 07:08:51 +0500

    Work with shaders

--- sourse/main.cpp
+++ sourse/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <ctime>
+#include <string>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    sf::RenderWindow window(sf::VideoMode(1080, 720), "smth");
+    window.setFramerateLimit(60);
+    sf::Event event;
+
+    sf::Shader shader;
+    if(!sf::Shader::isAvailable()) {
+        std::cout << "Hey! Shader not avaible!" << std::endl;
+        exit(1);
+    };
+    if(!shader.loadFromFile("../resourses/shaders/shader.frag", sf::Shader::Fragment)) {
+        std::cout << "Hey! Some problem with shader's loading!" << std::endl;
+        exit(1);
+    };
+
+    sf::RectangleShape rect(sf::Vector2f(1080, 720));
     
-    setlocale(LC_ALL, "rus");
-    std::cout << "Хей! :3" << std::endl;
-    
+    sf::RenderStates rs(&shader);
+
+
+    while(window.isOpen()) {
+        while(window.pollEvent(event)) {
+            switch(event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+
+        window.clear(sf::Color(0, 0, 0));
+        window.draw(rect, rs);
+        window.display();
+    }
+
     return 0;
 }
